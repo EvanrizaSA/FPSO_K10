de09e4b200471dc67b50685770da84a7688db6dd
EvanrizaSA <ariadi.evanriza@gmail.com>
2023-05-31 16:35:23 +0700

    tentangprodukfix

---
+++ resources/views/tentangproduk.blade.php
@@ -0,0 +1,30 @@
+@extends('layout')
+
+@section('content')
+<body class="max-w-xl mx-auto bg-slate-100">
+    <div class="mx-auto py-4 px-8 flex flex-col space-y-4 items-start justify-center">
+        <div class="inline-flex space-x-2.5 items-center justify-end w-28 h-1/2">
+            <img src="/assets/vertical-logo-text.png">
+        </div>
+    <p class="text-xs font-medium leading-none text-gray-700">Platform pencarian kendaraan hilang</p>
+    </div>
+    <div class="mx-auto py-4 px-8 flex flex-col space-y-4 items-start justify-center">
+        <div class="inline-flex space-x-4 items-center justify-start w-full">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+            </svg>
+        <p class="text-lg font-semibold leading-relaxed text-gray-700">Tentang Produk</p>
+        </div>
+        <p class="w-full text-xs leading-none text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    <div class="mx-auto py-4 px-8 pb-12 flex flex-col space-y-4 items-start justify-center">
+        <div class="inline-flex space-x-4 items-center justify-start w-full">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+            </svg>
+            <p class="text-lg font-semibold leading-relaxed text-gray-700">Tentang Pengembang</p>
+        </div>
+    <p class="w-full text-xs leading-none text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+</body>
+@endsection
